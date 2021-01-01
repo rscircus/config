2b565dfbe309fb8c631cdfddd3224e6e44c19a6c
Roland Siegbert <roland@siegbert.info>
2021-01-01 17:22:31 +0100

    Add tmux

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -6,3 +6,11 @@ mkdir -p ~/.config/nvim/
 mkdir -p ~/.local/share/nvim/site
 mkdir -p ~/.local/share/nvim/site/autoload
 wget -O ~/.local/share/nvim/site/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+## tmux
+
+mkdir -p ~/.tmux/plugins
+
+### Plugins
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
