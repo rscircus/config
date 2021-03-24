cc2e08e351d28f5dbac253c03677aa4756ab69e2
Roland Siegbert <roland@siegbert.info>
2021-03-24 05:54:18 +0100

    Add zsh-syntax-highlight to bootstrap

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -95,6 +95,11 @@ sudo apt install fasd
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+### further plugins
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+
 ### local customization
 
 touch ~/.zshrc_local
